44af802073b06104598ed66a5489a8f7eac6abba
Adriano Ferreira <adriano.ferreira@me.com>
2015-12-17 19:27:21 -0500

    Added install script

---
+++ install.sh
@@ -0,0 +1,73 @@
+#!/usr/bin/env bash
+
+set -e
+set -u
+
+# Fail fast if not on OS X
+
+if [[ "$(uname)" != 'Darwin' ]]; then
+  echo 'Ooops, this runs on OS X only.'
+  exit 1
+fi
+
+# Check if git is available
+
+if ! command -v 'git' &>/dev/null; then
+  echo -n 'Ooops, git not found. '
+  read -r -p 'Install it (y/n [n])? '
+
+  if [[ ! "${REPLY}" =~ ^[Yy]$ ]]; then
+    echo 'Ok then, nothing much to see here.'
+    exit 0
+  fi
+
+  if ! command -v 'xcode-select' &>/dev/null; then
+    echo 'Ooops, Xcode not found. '
+    read -r -p 'Install it (y/n [n])? '
+
+    if [[ ! "${REPLY}" =~ ^[Yy]$ ]]; then
+      echo 'Ok then, nothing much to see here.'
+      exit 0
+    fi
+
+    if ! 'xcode-select' --install &>/dev/null; then
+      exit 1
+    fi
+  fi
+fi
+
+# Define repo variables
+
+remote_repo='https://github.com/adrfer/bash-oh-my.git'
+local_repo="${HOME}/.bash-oh-my"
+
+# Check local repo
+
+if [[ -d "${local_repo}" ]]; then
+  echo -n "Backing up ${local_repo}..."
+  mv -f "${local_repo%/}"{," copy $(date +%s)"}
+  echo ' done.'
+fi
+
+# Clone the remote repo into the local repo
+
+echo -n "Cloning repo into ${local_repo}..."
+
+if git clone --quiet "${remote_repo}" "${local_repo}" &>/dev/null; then
+  echo ' done.'
+else
+  echo 'Ooops, there was an error, try again.'
+  exit 1
+fi
+
+# Define profile variables
+
+remote_profile="${local_repo}/Misc/Profile"
+local_profile="${HOME}/.bash_profile"
+
+# Link local profile
+
+ln -sf "${remote_profile}" "${local_profile}"
+
+echo 'Install successful, enjoy!'
+exit 0
